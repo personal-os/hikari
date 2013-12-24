180d1ac2026570cdd32ebc7486f664fe4a40c662
NetOperatorWibby <paul@dsgn.pw-software.com>
2013-12-24 08:17:03 -0500

    Added new off-screen menus

--- system/logic/classie.js
+++ system/logic/classie.js
@@ -2,11 +2,14 @@
  * classie - class helper functions
  * from bonzo https://github.com/ded/bonzo
  *
- * classie.has( elem, "my-class" ) -> true/false
- * classie.add( elem, "my-new-class" )
- * classie.remove( elem, "my-unwanted-class" )
- * classie.toggle( elem, "my-class" )
-**//* jshint browser: true, strict: true, undef: true */
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
+
 (function (window) {
 
 	"use strict";
@@ -22,27 +25,13 @@
 	var hasClass, addClass, removeClass;
 
 	if ("classList" in document.documentElement) {
-		hasClass = function (elem, c) {
-			return elem.classList.contains(c);
-		};
-		addClass = function (elem, c) {
-			elem.classList.add(c);
-		};
-		removeClass = function (elem, c) {
-			elem.classList.remove(c);
-		};
+		hasClass = function (elem, c) { return elem.classList.contains(c); };
+		addClass = function (elem, c) { elem.classList.add(c); };
+		removeClass = function (elem, c) { elem.classList.remove(c); };
 	} else {
-		hasClass = function (elem, c) {
-			return classReg(c).test(elem.className);
-		};
-		addClass = function (elem, c) {
-			if (!hasClass(elem, c)) {
-				elem.className = elem.className + " " + c;
-			}
-		};
-		removeClass = function (elem, c) {
-			elem.className = elem.className.replace(classReg(c), " ");
-		};
+		hasClass = function (elem, c) { return classReg(c).test(elem.className); };
+		addClass = function (elem, c) { if (!hasClass(elem, c)) { elem.className = elem.className + " " + c; } };
+		removeClass = function (elem, c) { elem.className = elem.className.replace(classReg(c), " "); };
 	}
 
 	function toggleClass(elem, c) {
@@ -50,7 +39,7 @@
 		fn(elem, c);
 	}
 
-	window.classie = {
+	var classie = {
 		// full names
 		hasClass: hasClass,
 		addClass: addClass,
@@ -62,5 +51,14 @@
 		remove: removeClass,
 		toggle: toggleClass
 	};
+
+	// transport
+	if (typeof define === "function" && define.amd) {
+		// AMD
+		define(classie);
+	} else {
+		// browser global
+		window.classie = classie;
+	}
 
 })(window);
