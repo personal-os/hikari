71344dc518fa1ff5c4b9ff3ad775b41923ec74cc
NetOperatorWibby <paul@dsgn.io>
2014-01-26 13:13:00 -0500

    Clicking "Chip Folder" actually does something

--- system/logic/gaea.js
+++ system/logic/gaea.js
@@ -12,3 +12,25 @@ $(document).ready(function () {
 	$(".hikari-content-inner").backstretch("system/shell/wallpaper/spring-sriram.jpg");
 
 });
+
+$("#select-chip-folder").click(function (e) {
+
+	// var wm = new Gaea.WindowManager();
+	// window.wm = wm; // For debugging reasons
+
+	$(".wm-space").append('<div id="chip-folder"></div>');
+
+	var chipWin = wm.createWindow.fromQuery("#chip-folder", {
+
+		title: "Chip Folder",
+		classname: "folder",
+		width: 600,
+		height: 300,
+		x: 85,
+		y: 120
+
+	});
+
+	e.preventDefault();
+
+});
