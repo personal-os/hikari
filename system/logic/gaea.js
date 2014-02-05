7e08a708c97cc096cb278009ee09bce183d6d878
NetOperatorWibby <paul@dsgn.io>
2014-02-05 18:25:49 -0500

    Lots of stuff going on

--- system/logic/gaea.js
+++ system/logic/gaea.js
@@ -232,7 +232,9 @@ $("#select-key-item").click(function (e) {
 // The Network × https://twitter.com/on_thenetwork
 $("#select-network").click(function (e) {
 
-	$(".wm-space").append('<section id="network">Hehe, this is gonna be awesome. And, take a bajillion years to create. You know how the Internet in MMBN and the anime was 3D? Yeah. Exactly. A bajillion years.</section>');
+	// $(".wm-space").append('<section id="network">Hehe, this is gonna be awesome. And, take a bajillion years to create. You know how the Internet in MMBN and the anime was 3D? Yeah. Exactly. A bajillion years.</section>');
+
+	$(".wm-space").append('<section id="network"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></section>');
 
 	var chipWin = wm.createWindow.fromQuery("#network", {
 
@@ -243,6 +245,11 @@ $("#select-network").click(function (e) {
 		x: 85,
 		y: 120
 
+	});
+
+	$("#network").parent().css({
+		"overflow-x": "hidden",
+		"overflow-y": "hidden"
 	});
 
 	e.preventDefault();
@@ -576,103 +583,128 @@ $(function () {
 	x = day.getHours();	// get the hour
 
 	if (x >= 0 && x < 1) {
-		$("html").removeClass().addClass("sand-babe");
+		// $("html").removeClass().addClass("sand-babe");
+		$("html").css("background", "linear-gradient(135deg, #b3cae5 12%, #dbdde4 46%, #e4e3e4 70%, #f7ddbb 94%, #efcab2 100%)");
 	}
 
 	if (x >= 1 && x < 2) {
-		$("html").removeClass().addClass("beach-towel");
+		// $("html").removeClass().addClass("beach-towel");
+		$("html").css("background", "linear-gradient(135deg, #5e7fb1 0%, #dce8f7 61%, #eff1f4 72%, #fce1a8 88%, #f7ec86 100%)");
 	}
 
 	if (x >= 2 && x < 3) {
-		$("html").removeClass().addClass("sky-blush");
+		// $("html").removeClass().addClass("sky-blush");
+		$("html").css("background", "linear-gradient(135deg, #8fb8ee 0%, #cbe2f4 40%, #dbe5eb 63%, #f9d3b8 83%, #e0b2a3 100%)");
 	}
 
 	if (x >= 3 && x < 4) {
-		$("html").removeClass().addClass("kentucky-dawn");
+		// $("html").removeClass().addClass("kentucky-dawn");
+		$("html").css("background", "linear-gradient(135deg, #b4ced8 17%, #d7e5d4 51%, #e2e8c9 72%, #f1e5b9 87%, #edd7ac 100%)");
 	}
 
 	if (x >= 4 && x < 5) {
-		$("html").removeClass().addClass("slate-sunrise");
+		// $("html").removeClass().addClass("slate-sunrise");
+		$("html").css("background", "linear-gradient(135deg, #506e90 0%, #7695aa 37%, #a7bdb8 56%, #e2e2b8 79%, #fdf998 100%)");
 	}
 
 	if (x >= 5 && x < 6) {
-		$("html").removeClass().addClass("blue-peach");
+		// $("html").removeClass().addClass("blue-peach");
+		$("html").css("background", "linear-gradient(135deg, #6bafd2 0%, #a4c8dc 38%, #d6cbca 58%, #eabc96 79%, #db8876 100%)");
 	}
 
 	if (x >= 6 && x < 7) {
-		$("html").removeClass().addClass("pixy-dust");
+		// $("html").removeClass().addClass("pixy-dust");
+		$("html").css("background", "linear-gradient(135deg, #95b3bf 0%, #c6cdd3 35%, #e5d8d9 64%, #f1e1d9 85%, #f3e1cd 100%)");
 	}
 
 	if (x >= 7 && x < 8) {
-		$("html").removeClass().addClass("rainbow-pudding");
+		// $("html").removeClass().addClass("rainbow-pudding");
+		$("html").css("background", "linear-gradient(135deg, #a7d3cb 0%, #bcc1c4 32%, #e5cab3 59%, #fee6c5 89%, #fdecd0 100%)");
 	}
 
 	if (x >= 8 && x < 9) {
-		$("html").removeClass().addClass("pale-elm");
+		// $("html").removeClass().addClass("pale-elm");
+		$("html").css("background", "linear-gradient(135deg, #bccacc 0%, #c7d8d6 26%, #d9ebe0 54%, #ebf9e3 78%, #f4f8d0 100%)");
 	}
 
 	if (x >= 9 && x < 10) {
-		$("html").removeClass().addClass("pastel-daylight");
+		// $("html").removeClass().addClass("pastel-daylight");
+		$("html").css("background", "linear-gradient(135deg, #a2e0f9 6%, #cef5fc 39%, #eafaeb 70%, #fefcd3 88%, #fdf4ba 100%)");
 	}
 
 	if (x >= 10 && x < 11) {
-		$("html").removeClass().addClass("dolphin");
+		// $("html").removeClass().addClass("dolphin");
+		$("html").css("background", "linear-gradient(135deg, #34a4ca 0%, #59d7dd 28%, #a8f2f0 59%, #d0f8ef 84%, #d6f6e1 100%)");
 	}
 
 	if (x >= 11 && x < 12) {
-		$("html").removeClass().addClass("field-day");
+		// $("html").removeClass().addClass("field-day");
+		$("html").css("background", "linear-gradient(135deg, #7696cd 0%, #8fb2e4 15%, #b0cff0 33%, #d7e5ec 69%, #dee0e7 92%)");
 	}
 
 	if (x >= 12 && x < 13) {
-		$("html").removeClass().addClass("seafoam-sun");
+		// $("html").removeClass().addClass("seafoam-sun");
+		$("html").css("background", "linear-gradient(135deg, #8dd6c3 6%, #c5e5e2 40%, #eafaeb 70%, #f9f7ca 88%, #fceea1 100%)");
 	}
 
 	if (x >= 13 && x < 14) {
-		$("html").removeClass().addClass("brilliant-sky");
+		// $("html").removeClass().addClass("brilliant-sky");
+		$("html").css("background", "linear-gradient(135deg, #4e72c7 0%, #6d9ed7 34%, #a4c8d5 67%, #b4d9e1 84%, #c4d9d6 100%)");
 	}
 
 	if (x >= 14 && x < 15) {
-		$("html").removeClass().addClass("mountain-time");
+		// $("html").removeClass().addClass("mountain-time");
+		$("html").css("background", "linear-gradient(135deg, #889db6 0%, #a5b8ce 20%, #c1cfdd 42%, #dee1e4 81%, #d5d1cf 100%)");
 	}
 
 	if (x >= 15 && x < 16) {
-		$("html").removeClass().addClass("candy-day");
+		// $("html").removeClass().addClass("candy-day");
+		$("html").css("background", "linear-gradient(135deg, #74bddb 0%, #a8d1eb 32%, #cddbf5 56%, #e4e6fb 73%, #f6f4f8 100%)");
 	}
 
 	if (x >= 16 && x < 17) {
-		$("html").removeClass().addClass("bruised-grape");
+		// $("html").removeClass().addClass("bruised-grape");
+		$("html").css("background", "linear-gradient(135deg, #ffe3c8 0%, #efad9e 45%, #c79797 65%, #a78a92 85%, #857d8d 100%)");
 	}
 
 	if (x >= 17 && x < 18) {
-		$("html").removeClass().addClass("violet-blush");
+		// $("html").removeClass().addClass("violet-blush");
+		$("html").css("background", "linear-gradient(135deg, #6f749e 0%, #9a8daf 31%, #d0a8b9 58%, #f8bbb1 80%, #fde6b1 100%)");
 	}
 
 	if (x >= 18 && x < 19) {
-		$("html").removeClass().addClass("lavender-skies");
+		// $("html").removeClass().addClass("lavender-skies");
+		$("html").css("background", "linear-gradient(135deg, #727288 6%, #8e889b 29%, #d3c2bd 70%, #f9d89a 89%, #f8c785 100%)");
 	}
 
 	if (x >= 19 && x < 20) {
-		$("html").removeClass().addClass("purple-sunset");
+		// $("html").removeClass().addClass("purple-sunset");
+		$("html").css("background", "linear-gradient(135deg, #7e74b2 9%, #b3a2c2 36%, #e2cdbe 66%, #f6cf97 85%, #f4a77a 100%)");
 	}
 
 	if (x >= 20 && x < 21) {
-		$("html").removeClass().addClass("brownie");
+		// $("html").removeClass().addClass("brownie");
+		$("html").css("background", "linear-gradient(135deg, #555351 0%, #555351 5%, #8d7b6c 40%, #cc9d7a 70%, #fff9aa 100%)");
 	}
 
 	if (x >= 21 && x < 22) {
-		$("html").removeClass().addClass("tequila-sunset");
+		// $("html").removeClass().addClass("tequila-sunset");
+		$("html").css("background", "linear-gradient(135deg, #47565f 0%, #5b625a 15%, #947461 38%, #f98056 70%, #f7ec86 100%)");
 	}
 
 	if (x >= 22 && x < 23) {
-		$("html").removeClass().addClass("sutro");
+		// $("html").removeClass().addClass("sutro");
+		$("html").css("background", "linear-gradient(135deg, #325571 0%, #8e9fa4 38%, #decab2 66%, #f2d580 78%, #ffa642 100%)");
 	}
 
 	if (x >= 23 && x < 24) {
-		$("html").removeClass().addClass("berry-juice");
+		// $("html").removeClass().addClass("berry-juice");
+		$("html").css("background", "linear-gradient(135deg, #c5d4d7 6%, #d6b98d 34%, #c99262 57%, #8c5962 80%, #43577e 100%)");
 	}
 
 	if (x >= 24 && x < 0) {
-		$("html").removeClass().addClass("indigo-magic");
+		// $("html").removeClass().addClass("indigo-magic");
+		$("html").css("background", "linear-gradient(135deg, #20202f 0%, #273550 16%, #416081 41%, #adacb2 78%, #eac3a2 100%)");
 	}
 
 });
