2bbffd867a83b8c89aaffecf3c8f7400415fd17b
NetOperatorWibby <paul@dsgn.io>
2014-03-18 22:20:33 -0400

    Authentication is getting on my nerves

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 var
 express = require("express"),
 app = express(),
+mongo = require("mongodb"), //
+monk = require("monk"), //
+// db = monk("localhost:28017/hikari"),
+db = monk("localhost:27017/hikari"), //
 passport = require("passport"),
 Sntp = require("sntp"),
 LocalStrategy = require("passport-local").Strategy;
@@ -17,63 +21,25 @@ app.configure(function () {
 
 });
 
-/*
-// Authentication configuration
-var passport = require("passport"), LocalStrategy = require("passport-local").Strategy;
-
-passport.use(new LocalStrategy(
-	function (username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-
-      if (err) { return done(err); }
-
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-
-      return done(null, user);
-
-    });
-  }
-));
-
 // Authentication route
 app.post("/login", passport.authenticate("local", { successRedirect: "/", failureRedirect: "/login" }));
-*/
-
-// Start up hikari!
-app.get("/login", function(req, res) {
-	res.render("login.html");
-});
-
-app.get("/", function(req, res) {
-	res.render("index.html");
-});
-
-/*
-app.get("/login", function (req, res) {
-	res.render("login.html", {
-		title: "Login",
-		user: req.user,
-		message: req.flash("error")
-	});
-});
-
-app.post("/login", passport.authenticate("local", {
-	successRedirect: "/worked",
-	failureRedirect: "/login",
-	failureFlash: true
-}));
 
 function ensureAuthenticated(req, res, next) {
 	if (req.isAuthenticated()) { return next(); }
 	res.redirect("/login");
 }
-*/
+
+// Super simple authentication
+// app.use(express.basicAuth("tadashi", "hikari"));
+
+// Start up hikari!
+app.get("/", function(req, res) {
+	res.render("index.html");
+});
+
+app.get("/login", function(req, res) {
+	res.render("login.html");
+});
 
 // Run "nodemon app"
 // 1343, for ACDC Town. Get it?
